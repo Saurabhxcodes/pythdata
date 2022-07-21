427b4dc0a8812d6477dcf3b8f5a9c400fc328eba
Saurabhxcodes <108970897+Saurabhxcodes@users.noreply.github.com>
2022-07-21 09:50:53 +0530

    basics

--- hello.py
+++ hello.py
@@ -1,1 +1,23 @@
-print("saurabh singh")
+#MAXIMUM ITEM IN ARRAY
+'''a = int(input("enter the number of arrsy="))
+t =list(map(int,input("enter the elements of array=").strip().split()))
+print(t)
+s = (max(t))
+print(s)'''
+
+#REVERSE THE ARRAY
+'''a=int(input("Number of elements in the array:-"))
+n=list(map(int, input("elements of array:-").strip().split()))
+print(n[: : -1])'''
+
+n=int(input("Number of elements in the array:-"))
+arr=list(map(int, input("elements of array:-").strip().split()))
+print(arr)
+for(int i=0; i<n; i++){
+    if arr[i]=arr[n-1]{
+        print("duplicate is present")
+    else:
+        print("no duplicate")
+    }
+
+}
