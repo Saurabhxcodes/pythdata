5cf9f00c5375b7b6b5ac50265ecf596b11d8047f
Saurabhxcodes <108970897+Saurabhxcodes@users.noreply.github.com>
2022-07-31 20:42:41 +0530

    strings use

---
+++ bltin.py
@@ -0,0 +1,5 @@
+for i in range(1000,16000):
+    print(f'{i}:{chr(i)}',end = "\t\t ")
+    if i % 10 == 0:
+        print()
+    
---
+++ str.py
@@ -0,0 +1,10 @@
+x = "SaurabhSingh"
+print( "at 5th index from starting ==>>",x[5])
+print( "at -7th index from backside ==>>>> ",x[-7])
+print( "at 4th index from starting ==>>>>>>",x[4])
+print( "at 2nd index from starting ==>>>>>>>>",x[2])
+print(x[3:8])
+x ="digipodium"
+print(x[0:4])
+print(x[:3])
+print(x[4:])
